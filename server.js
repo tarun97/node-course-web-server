7a01823364a4277bfebcc1302ac1fa8415575952
tarun97 <tarun.singh2897@gmail.com>
2018-05-28 19:40:38 +0530

    new project file added

--- server.js
+++ server.js
@@ -45,7 +45,11 @@ app.get('/about',(req,res)=>{
 
   });
 });
-
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle:'Project Page'
+  });
+});
 app.get('/bad',(req,res)=>{
   res.send({
     errormessage:'unable to process request'
